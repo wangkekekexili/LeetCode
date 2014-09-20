c8e7aa292a0ce357c223edd507863e6038d8a75d
Ke Wang <wangkekekexili@gmail.com>
2014-09-20 01:06:58 -0400

    Daily LeetCode

---
+++ LengthOfLastWord.java
@@ -0,0 +1,17 @@
+/**
+ * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
+ * return the length of last word in the string.
+ *
+ * @author Ke Wang
+ */
+
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        if (s==null||s=="")
+            return 0;
+        String[] parts = s.split(" +");
+        if (parts.length==0)
+            return 0;
+        return parts[parts.length-1].length();
+    }
+}
