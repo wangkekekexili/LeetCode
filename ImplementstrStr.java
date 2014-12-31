5c03de3a53ce66a525247fca66474dbdf3674a63
Ke Wang <wangkekekexili@gmail.com>
2014-12-30 22:43:42 -0500

    Daily LeetCode

---
+++ ImplementstrStr.java
@@ -0,0 +1,12 @@
+/**
+ * Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ * 
+ * @author Ke Wang
+ *
+ */
+
+public class ImplementstrStr {
+    public int strStr(String haystack, String needle) {
+    	return haystack.indexOf(needle);
+    }
+}
