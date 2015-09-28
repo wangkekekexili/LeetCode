6b99f5b277c9022b8112c6bc131a17c3b6485fb4
Ke Wang <wangkekekexili@gmail.com>
2015-09-28 19:18:05 -0400

    Balanced Binary Tree.

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,39 @@
+/**
+ * https://leetcode.com/problems/balanced-binary-tree/
+ * 
+ * @author kewang
+ *
+ */
+public class BalancedBinaryTree {
+	
+	private boolean ifBalance = true;
+	
+	private int height(TreeNode node) {
+		if (ifBalance == false) {
+			return -1;
+		}
+		if (node == null) {
+			return 0;
+		}
+		int leftHeight = height(node.left);
+		int rightHeight = height(node.right);
+		if (Math.abs(leftHeight - rightHeight) <= 1) {
+			return 1 + Math.max(leftHeight, rightHeight);
+		} else {
+			ifBalance = false;
+			return -1;
+		}
+	}
+	
+    public boolean isBalanced(TreeNode root) {
+    	if (root == null) {
+    		return true;
+    	}
+    	height(root);
+    	if (ifBalance == true) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+}
