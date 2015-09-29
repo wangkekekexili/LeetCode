9a65745e1e8f8aa8bd081a9df98eb686469450da
Ke Wang <wangkekekexili@gmail.com>
2015-09-28 20:26:55 -0400

    Fix bug.

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -7,25 +7,26 @@
  */
 
 public class MaximumSubarray {
-    public int maxSubArray(int[] A) {
-    	if (A == null || A.length == 0) {
+    public int maxSubArray(int[] nums) {
+    	if (nums == null || nums.length == 0) {
     		return 0;
     	}
-    	if (A.length == 1) {
-    		return A[0];
+    	if (nums.length == 1) {
+    		return nums[0];
     	}
-    	int max = A[0];
-    	int currentMax = A[0];
+    	int max = nums[0];
+    	int currentMax = nums[0];
     	
-    	for (int index = 1; index < A.length; index++) {
-    		if (currentMax + A[index] > A[index]) {
-    			currentMax += A[index];
+    	for (int index = 1; index < nums.length; index++) {
+    		if (currentMax + nums[index] > nums[index]) {
+    			currentMax += nums[index];
     		} else {
-    			currentMax = A[index];
+    			currentMax = nums[index];
     		}
     		if (currentMax > max) {
     			max = currentMax;
     		}
     	}
+    	return max;
     }	
 }
